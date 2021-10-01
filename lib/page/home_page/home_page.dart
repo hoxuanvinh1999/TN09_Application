3e4eb4d2c18d84cbae24601be8b2551a29de53af
Ho Xuan Vinh <xuan-vinh.ho@etu.utc.fr>
2021-10-01 10:58:40 +0200

    begin with web

--- lib/page/home_page/home_page.dart
+++ lib/page/home_page/home_page.dart
@@ -34,7 +34,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final applicationBloc = Provider.of<ApplicationBloc>(context);
+    // final applicationBloc = Provider.of<ApplicationBloc>(context);
     return WillPopScope(
       onWillPop: () async {
         final goback = await _onBackPressed(context);
@@ -46,32 +46,34 @@ class HomeScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text('TN09 Demo Applicaiton'),
         ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: RaisedButton(
-                  child: Text(
-                    'Sign Out',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
+        body: SingleChildScrollView(
+          child: Card(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: RaisedButton(
+                    child: Text(
+                      'Sign Out',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
+                    onPressed: () {
+                      auth.signOut();
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => LoginScreen()));
+                    },
+                    color: Theme.of(context).primaryColor,
                   ),
-                  onPressed: () {
-                    auth.signOut();
-                    Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) => LoginScreen()));
-                  },
-                  color: Theme.of(context).primaryColor,
                 ),
-              ),
-              Image.asset('assets/logo_1.jpg'),
-              Text('Demo app homepage'),
-            ],
+                Image.asset('assets/logo_1.jpg'),
+                Text('Demo app homepage'),
+              ],
+            ),
           ),
         ),
       ),
